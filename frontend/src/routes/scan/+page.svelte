f9866a2b75c78768ea44027dae42da2be6adf093
Kellories <jeremytswong@gmail.com>
2024-04-06 21:21:57 +0800

    feat: QR Code CSS

--- frontend/src/routes/scan/+page.svelte
+++ frontend/src/routes/scan/+page.svelte
@@ -5,27 +5,47 @@
   let scanning = false
 
   let html5Qrcode
-
+  let innerWidth = 0
+  let innerHeight = 0
   onMount(async()=>{
     await init()
     await start()
+    console.log(innerWidth,innerHeight)
   })
+
 
   async function init() {
       html5Qrcode = new Html5Qrcode('reader')
   }
 
   async function start() {
-      html5Qrcode.start(
-          { facingMode: 'environment' },
-          {
-              fps: 10,
-              qrbox: { width: 200, height:200 },
-          },
-          onScanSuccess,
-          onScanFailure
-      )
-      scanning = true
+
+    if(innerWidth < 768){
+
+        html5Qrcode.start(
+            { facingMode: 'environment' },
+            {
+                fps: 10,
+                qrbox: { width: 100, height:100 },
+            },
+            onScanSuccess,
+            onScanFailure
+        )
+        scanning = true
+
+    }else{
+        html5Qrcode.start(
+            { facingMode: 'environment' },
+            {
+                fps: 10,
+                qrbox: { width: 200, height:200 },
+            },
+            onScanSuccess,
+            onScanFailure
+        )
+        scanning = true
+    }
+    
   }
 
   async function stop() {
@@ -42,7 +62,7 @@
       console.warn(`Code scan error = ${error}`)
   }
 </script>
-
+<!-- 
 <style>
   main {
       display: flex;
@@ -55,13 +75,18 @@
       min-height: 80%;
       background-color: black;
   }
-</style>
-
-<main>
-  <reader  id="reader"/>
-  {#if scanning}
-      <button on:click={stop}>stop</button>
-  {:else}
-      <button on:click={start}>start</button>
-  {/if}
+</style> -->
+<svelte:window bind:innerWidth bind:innerHeight/>
+ <!--  desktop view -->
+{#if innerWidth > 768 }
+    
+<main class = 'flex flex-col items-center text-center justify-center min-h-[50%] max-h-[50%] mt-[2rem]'>
+    <p class = 'font-semibold py-[1rem] text-xl'>Scan QR to add device.</p>
+  <reader class ="w-[50%] min-h-[50%] max-h-[50%]" id="reader"/>
 </main>
+{:else}
+<main class = 'flex flex-col justify-center text-center min-h-[50%] max-h-[50%] mt-[9rem]'>
+    <p class = 'font-semibold py-[1rem]'>Scan QR to add device.</p>
+    <reader class ="w-screen" id="reader"/>
+  </main>   
+{/if}
