0ecb0dc8bbd46771f00a68fe38d2e4dad6742eeb
avicity7 <karlorjalo@gmail.com>
2024-04-07 00:59:07 +0800

    feat: Socket io in /devices

--- frontend/src/routes/devices/+page.svelte
+++ frontend/src/routes/devices/+page.svelte
@@ -1,35 +1,40 @@
-<script>
-    import {onMount} from 'svelte'
-    import { page } from '$app/stores';
-    import DeviceImage from '../../assets/render.png'
-    export let data
-    let { backend_uri, session } = data;
-    let account
-    let devices = []
-    let notLoaded;
-    const getProfile = async () => {
-    const response = await fetch(
-      `${backend_uri}:3000/api/auth/getUser?email=${session?.user?.email}`
-    );
-    const parsed = await response.json();
-    account = parsed.rows[0]
-		notLoaded = false
-    };
-     const getDevices =  async () => {
-      const response = await fetch(`${backend_uri}:3000/api/iot/getDevices?email=${session?.user?.email}`)
-      devices = await response.json()
-      console.log(devices)
-    
-    }
-    onMount(async () => {
-		if (session != undefined) {
-			await getProfile();
+<script lang="ts">
+  import { io } from "socket.io-client";
+  import {onMount} from 'svelte'
+  import { page } from '$app/stores';
+  import DeviceImage from '../../assets/render.png'
+  export let data
+  let { backend_uri, session } = data;
+  let account
+  let devices: [{data: number | null, id: string | null}] = [{data: null, id: null}]
+  let notLoaded;
+  const getProfile = async () => {
+  const response = await fetch (
+    `${backend_uri}:3000/api/auth/getUser?email=${session?.user?.email}`
+  );
+  const parsed = await response.json();
+  account = parsed.rows[0]
+  notLoaded = false
+  };
+  const getDevices =  async () => {
+    const response = await fetch(`${backend_uri}:3000/api/iot/getDevices?email=${session?.user?.email}`)
+    devices = await response.json()
+  }
+  onMount(async () => {
+    if (session != undefined) {
+      await getProfile();
             await getDevices();
-		} else {
-			notLoaded = false;
-		}
-	});
-
+    } else {
+      notLoaded = false;
+    }
+  });
+  const socket = io(`${backend_uri}:3000`,{
+		transports: ['websocket']
+  })
+  socket.on(session?.user?.email, ({data, id}: {data: number, id: string}) => {
+    devices.find(obj => obj.id == id)!.data = data
+    devices = devices
+  })
 </script>
 
 <h1 class = 'text-center p-[2rem] text-3xl font-semibold'> devices </h1>
